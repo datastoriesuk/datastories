7b8ae0942ff10e43da7007e65fb6ac70210771a2
Giannis <ghara@windowslive.com>
2017-11-21 16:18:40 +0000

    Store the url
    Will answer unique urls question.

--- wp3/master/urldate.py
+++ wp3/master/urldate.py
@@ -5,9 +5,8 @@ from datetime import datetime
 data=pd.read_csv(os.path.abspath("datastories\wp3\data\@guardian.csv"),encoding="ISO-8859-1")
 
 data=data[data.text.str.contains(r'http')]
-data["url_creation"]=0
-data["tweet_creation"]=0
-data["time_diff_minutes"]=0
+data["url"]=""
+data["url_creation"], data["tweet_creation"], data["time_diff_minutes"] = [0,0,0]
 
 for i in data.index:
     try:
@@ -16,8 +15,9 @@ for i in data.index:
         resp = session.head(link, allow_redirects=True).url
         page = requests.get(resp)
         soup = BeautifulSoup(page.content, 'html.parser')
-        time = soup.find_all('time', class_="content__dateline-wpd js-wpd")[0].get("datetime")
-        data["url_creation"][i] = datetime.strptime(time,"%Y-%m-%dT%H:%M:%S+0000")
+        timeofcr = soup.find_all('time', class_="content__dateline-wpd js-wpd")[0].get("datetime")
+        data["url"][i] = resp
+        data["url_creation"][i] = datetime.strptime(timeofcr,"%Y-%m-%dT%H:%M:%S+0000")
         data["tweet_creation"][i] = datetime.strptime(data["created_at"][i], '%a %b %d %H:%M:%S +0000 %Y')
         data["time_diff_minutes"][i] = (data["tweet_creation"][i] - data["url_creation"][i]).total_seconds() / 60.0
     except Exception as e:
