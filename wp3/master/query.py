06101f4e33ecd4cb71e9352d09759470b84733b0
Giannis <ghara@windowslive.com>
2017-11-21 14:11:51 +0000

    Hid keys

--- wp3/master/query.py
+++ wp3/master/query.py
@@ -1,29 +1,34 @@
-import tweepy,csv,sys,pandas as pd
+import tweepy,sys,pandas as pd
 
-# Twitter auth process
-auth = tweepy.OAuthHandler('mJ4E1iTrsDdaiiwHRHz6BnPk1', 'ANKgwrxpizaUjzThUj4xsakGu5j6VVDf8QpL0CN7kNYx9yxYSU')
-auth.set_access_token("919910347972268032-dkUsnh8aD3dMACBLDyMdPetKSmWbXDi", "fYH5hZgnvtLpqpmbX4rseiBpW4ZZeCONndiV5CXD47kbl")
+keys = [line.rstrip('\n') for line in open("..\\keys.txt")]
+auth = tweepy.OAuthHandler(keys[0], keys[1])
+auth.set_access_token(keys[2], keys[3])
 api = tweepy.API(auth)
 
-qr=sys.argv[1]
+#qr=sys.argv[1]
+qr="@guardian"
 
 def query(qr):
-    # Create a list to store items
-    list = []
+    twlist = []
     while True:
             try:
                 if qr.startswith("@"):
                     for item in tweepy.Cursor(api.user_timeline,screen_name=qr.replace("@",""),lang="en").items():
-                        list.append(item._json)
+                        twlist.append(item._json)
                 else:
                     for item in tweepy.Cursor(api.search,q=qr,rpp=100,result_type="recent",include_entities=True,lang="en").items():
-                        list.append(item)
+                        twlist.append(item)
             except Exception as e:
                 if "e==Twitter error response: status code = 429":
                     print("Request limit reached")
                     # End after first limit
                     break
-                    # Wait for request reset
-                    time.sleep(901)
+                    # # Wait for request limit reset
+                    # time.sleep(910)
 
-pd.DataFrame(list).to_csv("datastories/wp3/data/"+qr+".csv")
+def main():
+    query(qr)
+    pd.DataFrame(twlist).to_csv("datastories/wp3/data/" + qr + ".csv")
+
+if __name__ == '__main__':
+    sys.exit(main())
